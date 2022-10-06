56a1d94944189e6d33878c8cf4abaaa89eca4b03
mmoraesspbr <96797910+mmoraesspbr@users.noreply.github.com>
2022-10-05 22:43:20 -0300

    SonarCloud - SAST

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Marcelo Moraes"
+    return "Hello World - Marcelo Moraes 05/10/2022"
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World - Marcelo Moraes")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World - Marcelo Moraes 05/10/2022")
