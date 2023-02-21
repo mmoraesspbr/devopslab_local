e5fd5b8060495d4a59ea9a4f7b9a636755524523
mmoraesspbr <mmoraes_sp@hotmail.com>
2023-02-21 17:20:21 -0300

    LocalMarceloV3

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Marcelo 19/02/2023 - Pipeline DevOps - v1"
+    return "Marcelo local 21/02/2023 - Pipeline DevOps - v3"
 
 if __name__ == '__main__':
     app.run()
